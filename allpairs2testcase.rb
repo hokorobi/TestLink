73802a1bb4dcfe262da3e3eb3f256ba85ab9b8a4
hokorobi <hokorobi@.(none)>
2009-09-25 22:15:49 +0900

    apply to shiumachi patch

--- allpairs2testcase.rb
+++ allpairs2testcase.rb
@@ -11,25 +11,23 @@ require 'kconv'
 require 'csv2testcace'
 
 class ApTestcase
-  attr_accessor :items,:testcase
+  attr_accessor :items,:testcase,:expectedResult
   @items
   @testcase
+  @expectedResult
   def initialize
     @items=[]
     @testcase = []
+    @expectedResult = ""
   end
   def print
     s="<TABLE>"
-    s=s + "<TR>"
     for i in 0..@items.size-2
+      s=s + "<TR>"
       s=s+"<TD>#{@items[i].split}</TD>"
-    end
-    s=s + "</TR>"
-    s=s + "<TR>"
-    for i in 0..@testcase.size-2
       s=s+"<TD>#{@testcase[i].split}</TD>"
+      s=s + "</TR>"
     end
-    s=s + "</TR>"
     s=s + "</TABLE>"
   end
   def getTitle
@@ -48,21 +46,41 @@ class Allpairs2Testcase
   attr_accessor :allpairsTestcase
   @allpairsTestcase
   @csv2testcase
+  @details
+  @summary
   def initialize
     @allpairsTestcase=AllpairsTestcase.new
     @csv2testcase=Csv2testcase.new
+    @details=""
+    @summary=""
   end
   def readFile(infile)
     f = File.open(infile,"r")
-    s=f.gets
-    s=f.gets
+    # skip first 2 lines
+    # if there are some information about "details" and/or "summary", get them.
+    s = f.gets.split("||")
+    if(s.first == "details")
+      @details = s[1]
+    end
+    s = f.gets.split("||")
+    if(s.first == "summary")
+      @summary = s[1]
+    end
+    # get a label line
     s=f.gets
     items=s.split("\t")
+    useExpRes = false
+    if items.last.chomp == "ExpectedResult" then
+      items.pop
+      useExpRes = true
+    end
     s=f.gets
     while s !="\n"
       tc=ApTestcase.new
       tc.items=items
-      tc.testcase=s.split("\t")
+      t=s.split("\t")
+      tc.expectedResult = t.pop if useExpRes
+      tc.testcase = t
       @allpairsTestcase.testcase << tc
       s=f.gets
     end
@@ -71,9 +89,9 @@ class Allpairs2Testcase
 	def initTestSuite(name,details)
     @csv2testcase.initTestSuite(name,details)
 	end
-  def convXML(name_prefix="Test case No.",summary_prefix="",steps_prefix="",expectedresults_prefix="")
+  def convXML(name_prefix="Test case No.",summary_prefix=@summary,steps_prefix="",expectedresults_prefix="")
     @allpairsTestcase.testcase.each{|tc|
-      @csv2testcase.addTestcase(name_prefix + tc.getTitle,summary_prefix + tc.print,steps_prefix + tc.print,expectedresults_prefix)
+      @csv2testcase.addTestcase(name_prefix + tc.getTitle,summary_prefix,steps_prefix + tc.print,expectedresults_prefix + tc.expectedResult)
     }
   end
   def writeFile(outfile)
@@ -81,7 +99,7 @@ class Allpairs2Testcase
   end
   def main(infile,outfile)
 		readFile(infile)
-    initTestSuite(infile,"")
+    initTestSuite(infile,@details)
     convXML
 		writeFile(outfile)
   end
